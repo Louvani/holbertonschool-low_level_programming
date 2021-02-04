052e697fc2f2a19058377b383c9bddb7c7161eeb
Louvani <paula.louvani@gmail.com>
2021-02-04 14:52:53 -0500

    Commpile task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,9 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	int a;
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
