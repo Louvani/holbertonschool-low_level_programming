56b64b952e755998f3bf46f4844d5461edfab251
Louvani <paula.louvani@gmail.com>
2021-02-04 19:40:24 -0500

    Create task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry ponit
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
