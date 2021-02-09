f48f0e24cca5f067ca2299f060365570c59e57da
Louvani <paula.louvani@gmail.com>
2021-02-09 07:13:40 -0500

    testing task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *main - Entry point
+ *Return: On success 0.
+ */
+void main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
