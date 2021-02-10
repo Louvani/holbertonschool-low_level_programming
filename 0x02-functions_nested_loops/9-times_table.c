3129ac90af4b1a8849520ba628d75c361dc8e1a2
Louvani <paula.louvani@gmail.com>
2021-02-09 22:50:34 -0500

    Task 9 more and more spaces

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,13 +25,16 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-			else if (k <= 10)
+			else if (k <= 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
 			{
 				_putchar(k + '0');
 			}
-			
 		}
-	_putchar('\n');	
+	_putchar('\n');
 	}
-
 }
