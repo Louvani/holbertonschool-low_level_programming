cc0d8697455935be837843c2862ca2087a259e5d
Louvani <paula.louvani@gmail.com>
2021-02-11 22:14:57 -0500

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *more_numbers - Entry point
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; 0 < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + 48);
+			}
+			_putchar((x % 10) + 48);
+		}
+	_putchar(10);
+	}
+}
