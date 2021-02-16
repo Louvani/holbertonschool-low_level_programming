9536bc2f536d30f186a589712c607d82d80c166c
Louvani <paula.louvani@gmail.com>
2021-02-16 14:22:44 -0500

    Correct Betty for the funcitón _strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,14 @@
 /**
  * _strlen - returns the length of a string
  *@s: variable with the string
- *@l: Kepp the length of the sttring
  *
- * Return: always 0.
+ * Return: always l.
  */
-
 int _strlen(char *s)
 {
 	int l;
 
 	l = 0;
-
 	while (*s != '\0')
 	{
 		l++;
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _puts - function that prints a string
+ *@str: 
+ *@l: 
+ *
+ * Return: always 0.
+ */
+
+void _puts(char *str)
+{
+	int l;
+
+	l = 0;
+
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
