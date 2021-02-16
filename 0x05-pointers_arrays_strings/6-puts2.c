2a89ce579091437dd8fa7315069a179039f3d39f
Louvani <paula.louvani@gmail.com>
2021-02-16 17:02:18 -0500

    function that prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *puts2 - function that prints every other character of a string
+ *@str: String
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str+=2;
+	}
+	_putchar(10);
+}
