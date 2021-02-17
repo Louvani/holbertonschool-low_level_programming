da858677a77b94e6bef4eded782c61ffb69c8831
Louvani <paula.louvani@gmail.com>
2021-02-16 19:45:09 -0500

    Betty is perfect in task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,16 +2,18 @@
 
 /**
  *print_rev - Functión that prints a string, in reverse
- *@
+ *@s: Variable that keeps the string
  */
 void print_rev(char *s)
 {
 	int l;
 	int m;
+
 	l = 0;
+
 	while (*s != '\0')
- 	{
- 		l++;
+	{
+		l++;
 		s++;
 	}
 	for (m = l; m >= 0; --m)
