765e6c699b0ed31f336038ff193f36c44d8daa65
Louvani <paula.louvani@gmail.com>
2021-02-18 22:30:39 -0500

    3. strcmp adding break

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,19 +9,20 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (*s1 != *s2)
 		{
-			j = 1;
+			i = 1;
 			break;
 		}
-		i++;
+		s1++;
+		s2++;
 	}
-	if (j == 0 && s1[i] == '\0' && s2[i] == '\0')
+	if (i == 0)
 		return (0);
 	else
-		return (s1[i] - s2[i]);
+		return (*s1 - *s2);
 }
