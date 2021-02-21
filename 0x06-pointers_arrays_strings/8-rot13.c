434818164f9b0add8a56dabc0afadaf5b1e9136f
Louvani <paula.louvani@gmail.com>
2021-02-20 21:20:38 -0500

    task 8 + vim

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,15 +9,14 @@ char *rot13(char *str)
 {
 	int len, len2;
 	char alph[] = "abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM";
- 	// str[0] R
+
 	for (len = 0; str[len] != '\0'; len++)
 	{
-		//0 alph[0] a
 		for (len2 = 0; alph[len2] != '\0'; len2++)
 		{
 			if (str[len] == alph[len2])
 			{
-				str[len] = alph[len2 + 13];
+				str[len] = alph[len2 += 13];
 				break;
 			}
 		}
