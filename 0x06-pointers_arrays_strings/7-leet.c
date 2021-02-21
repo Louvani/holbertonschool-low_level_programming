e943723f42307c296099f13fa210733ce20c3fae
Louvani <paula.louvani@gmail.com>
2021-02-20 19:07:06 -0500

    Task 7 + betty

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,6 +2,8 @@
 
 /**
  * *leet - function that encodes a string into 1337
+ * @str: variable
+ * Return: Always str.
  */
 char *leet(char *str)
 {
@@ -9,7 +11,7 @@ char *leet(char *str)
 	char alph[] = "aAeEoOtTlL";
 	char num[] = "4433007711";
 
-	for (len = 0; str[len] != '\0'; len ++)
+	for (len = 0; str[len] != '\0'; len++)
 	{
 		for (len2 = 0; alph[len2] != '\0'; len2++)
 		{
