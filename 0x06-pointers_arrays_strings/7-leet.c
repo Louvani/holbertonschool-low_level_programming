4784c79d0732897521296e31e6cee9d008ed6d86
Louvani <paula.louvani@gmail.com>
2021-02-20 19:05:24 -0500

    Task 7 function that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * *leet - function that encodes a string into 1337
+ */
+char *leet(char *str)
+{
+	int len, len2;
+	char alph[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	for (len = 0; str[len] != '\0'; len ++)
+	{
+		for (len2 = 0; alph[len2] != '\0'; len2++)
+		{
+			if (str[len] == alph[len2])
+			{
+				str[len] = num[len2];
+			}
+		}
+	}
+	return (str);
+}
