cd04497fb259cda049bc40b7d3096585e98f7cdb
Louvani <paula.louvani@gmail.com>
2021-02-22 23:39:11 -0500

    task 8 + vim

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -3,8 +3,7 @@
 /**
  * set_string - function that sets the value of a pointer to a char
  * @s: Array
- * @size: size of the array
- * Return:  the num of bytes in the initial segment of s.
+ * @to: second array
  */
 void set_string(char **s, char *to)
 {
