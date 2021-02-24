0b44ffd1acc86c4726360a156b8773570e0bdb1a
Louvani <paula.louvani@gmail.com>
2021-02-24 13:46:54 -0500

    Creat Readme, Holberton.h and task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string.
+ * @s: String
+ * Return: nothing.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
