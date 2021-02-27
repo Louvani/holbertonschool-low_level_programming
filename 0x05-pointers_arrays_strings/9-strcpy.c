45406a87dd43d9e4dbfb96b162ec415e1b9c934f
Louvani <paula.louvani@gmail.com>
2021-02-27 15:15:50 -0500

    Task 9 retun null if src is empty in for loop

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,13 @@ char *_strcpy(char *dest, char *src)
 	int i;
 	int len = _strlen(src);
 
-	if (len == 0)
-	{
-		dest = '\0';
-	}
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+		for ( ; i < len; i++)
+		{
+			dest[i] = '\0';
+		}
 	}
 	return (dest);
 }
@@ -33,6 +33,7 @@ int _strlen(char *s)
 	int l;
 
 	l = 0;
+	
 	while (*s != '\0')
 	{
 		l++;
