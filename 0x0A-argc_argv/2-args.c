2540318d370add47966324a2a1c5e71516ade67b
Louvani <paula.louvani@gmail.com>
2021-02-27 21:35:04 -0500

    print arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc:Counter of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
