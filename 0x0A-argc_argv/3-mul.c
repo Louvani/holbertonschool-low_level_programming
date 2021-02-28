68dad72d2419472e7d778068bf4e3c141ac5ff4e
Louvani <paula.louvani@gmail.com>
2021-02-28 13:51:52 -0500

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "holberton.h"
 #include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
 
 /**
  * main - prints the number of arguments passed into it.
@@ -13,7 +15,7 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc == 1)
+	if (argc == 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -23,7 +25,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			mul = mul * strtol(argv[i], NULL, 10);
+			mul = mul * atoi(argv[i]);
 		}
 		printf("%d\n", mul);
 	}
