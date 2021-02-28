51a969b2cb8fd23d391ee7fed661d89cf028dd0f
Louvani <paula.louvani@gmail.com>
2021-02-27 21:46:39 -0500

    3.mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc:Counter of arguments
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
