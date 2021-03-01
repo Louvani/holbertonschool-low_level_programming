45304d9bff8d5d9f42d19d439d96a933dd25930e
Louvani <paula.louvani@gmail.com>
2021-02-28 23:40:16 -0500

    TAsk 100 Change money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+#include <ctype.h>
+
+int change(int cent, int *coin);
+
+/**
+ * main - prints the minimum num of coins to make change for an amount of money
+ * @argc:Counter of arguments
+ * @argv: arguments
+ * Return: 0 if have more than 1 arguments.
+ */
+
+int main(int argc, char *argv[])
+{
+	int coin[5] = {25, 10, 5, 2, 1};
+	int i, cent, result;
+
+	for (i = 0; i < argc; i++)
+	{
+		cent = atoi(argv[i]);
+		result = change(cent, coin);
+	}
+	printf("%d\n", result);
+	return (0);
+}
+
+/**
+ * change - change for an amount of money
+ * @cent: Cents recived
+ * @coin: coins
+ * Return: value of moneys.
+ */
+int change(int cent, int *coin)
+{
+	if ((cent % *coin) == 0)
+	{
+		return (cent / *coin);
+	}
+	return ((cent / *coin) + (change((cent % *coin), (coin + 1))));
+}
