d8efe25791c97589a2a203bce02848ae31c1b288
Louvani <paula.louvani@gmail.com>
2021-03-02 16:08:17 -0500

    Task 0 needs a loop
    while (i < size)
    {
    	a[i] = c;
    	i++;
    }
    	return (a);

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,14 +7,22 @@
  * @c: charcater to initialize
  * Return: variable a
  */
+
 char *create_array(unsigned int size, char c)
 {
 	char *a;
+	unsigned int i = 0;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 	a = malloc(sizeof(c) * size);
+
+	while (i < size)
+	{
+		a[i] = c;
+		i++;
+	}
 	return (a);
 }
