4b1e2770e06ed7bb1492e4ebfa3149ff2d105367
Louvani <paula.louvani@gmail.com>
2021-03-04 00:39:18 -0500

    Task 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,21 +1,58 @@
 #include <stdio.h>
 #include "holberton.h"
 #include <stdlib.h>
+int _strlen(char *s);
 
 /**
  *  *argstostr - prints the number of arguments passed into it.
  * @ac:Counter of arguments
  * @av: arguments
- * Return: 0
+ * Return: a
  */
 
 char *argstostr(int ac, char **av)
 {
-	int i;
+	int i, j, len = 0, k, l = 0;
+	char *a;
 
 	for (i = 0; i < ac; i++)
 	{
-		printf("%s\n", av[i]);
+		len += _strlen(av[i]) + 1;
 	}
-	return (av[0]);
+	a = malloc(len + 1);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j < ac; j++)
+	{
+		for (k = 0; av != '\0'; k++)
+		{
+			a[l] = av[j][k];
+			l++;
+		}
+		a[l] = '\n';
+	}
+	a[l + 1] = '\0';
+	return (a);
+}
+
+/**
+ * _strlen - returns the length of a string
+ *@s: variable with the string
+ *
+ * Return: always l.
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = 0;
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
 }
