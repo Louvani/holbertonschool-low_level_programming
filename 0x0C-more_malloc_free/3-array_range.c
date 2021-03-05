23dd24060fe7aed9486af7b70369337263c1d158
Louvani <paula.louvani@gmail.com>
2021-03-04 21:04:04 -0500

    Task3 + betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,11 +4,10 @@
 /**
  * array_range - function that creates an array of integers.
  * @min: min number of a range
- * @maz: max number of a range
+ * @max: max number of a range
  *
  * Return: array.
  */
-
 int *array_range(int min, int max)
 {
 	int *ptr, minaux = min;
