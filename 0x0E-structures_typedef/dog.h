b04d3aeb7d032d5fef1bfad59e0527931b3bd964
Louvani <paula.louvani@gmail.com>
2021-03-08 15:39:10 -0500

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * struct dog - data of pets
+ * @name: Name of the pet
+ * @age: the age of the pet
+ * @owner: TThe name of the owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*HOLBERTON_H*/
