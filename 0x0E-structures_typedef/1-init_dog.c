8d67eea102f52334d52ba6985a4c5eb18cd5e67a
Louvani <paula.louvani@gmail.com>
2021-03-08 18:09:17 -0500

    Task 1
    Don't need specify *d as struct type

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type struct dog.
+ * @d: pointer to My_dog
+ * @name: Name of the pet
+ * @age: age of the pet
+ * @owner: owner of the pet
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
