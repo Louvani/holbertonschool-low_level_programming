45de403199af5e967a78972eeafbdb7877d66e5f
Louvani <paula.louvani@gmail.com>
2021-03-08 20:13:16 -0500

    Task 2 condicionales independientes otra vez

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,20 +6,18 @@
  * @d: adrres of my_dog
  * Return: Always 0.
  */
+
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
-		printf("Name: %f\n", d->age);
-		if (d->owner != NULL)
-			printf("Name: %s\n", d->owner);
-		else
-			printf("Name: (nil)\n");
-	}
+	if (d == NULL)
+		return;
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
 	else
-			return;
+		printf("Name: (nil)\n");
+	printf("Name: %f\n", d->age);
+	if (d->owner != NULL)
+		printf("Name: %s\n", d->owner);
+	else
+		printf("Name: (nil)\n");
 }
