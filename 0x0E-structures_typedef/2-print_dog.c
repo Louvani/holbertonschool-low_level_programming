b67d9d63ed02ee36806f477e6b33810a0ee5e764
Louvani <paula.louvani@gmail.com>
2021-03-08 19:30:18 -0500

    Task2 everithing in one condintion

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * print_dog - function that prints a struct dog.
  * @d: adrres of my_dog
  * Return: Always 0.
  */
-
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-
-	if (d->name != NULL)
-		printf("Name: %s\n", d->name);
-	else
-		printf("Name: (nil)\n");
-	printf("Name: %lf\n", d->age);
-	if (d->owner != NULL)
-		printf("Name: %s\n", d->owner);
-	else
-		printf("Name: (nil)\n");
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Name: %lf\n", d->age);
+		if (d->owner != NULL)
+			printf("Name: %s\n", d->owner);
+		else
+			printf("Name: (nil)\n");
+	}
 }
