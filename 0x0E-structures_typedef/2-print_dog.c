2545488761972f8be21662aa150da88dd5b61f78
Louvani <paula.louvani@gmail.com>
2021-03-08 19:32:32 -0500

    Task2 %lf -> %f

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		printf("Name: %lf\n", d->age);
+		printf("Name: %f\n", d->age);
 		if (d->owner != NULL)
 			printf("Name: %s\n", d->owner);
 		else
