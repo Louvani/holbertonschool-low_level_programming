576bd6070f597596c1cd7638753f4745c2139727
Louvani <paula.louvani@gmail.com>
2021-03-08 21:34:37 -0500

    no verifica en princiop d como nullo

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+
 /**
  * free_dog - frees dogs
  * @d: my dog
@@ -7,8 +8,6 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
 	if (d != NULL)
 	{
 		if (d->name != NULL)
