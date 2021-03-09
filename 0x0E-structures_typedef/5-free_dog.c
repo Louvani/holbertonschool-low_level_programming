e6b5242fe02961fb6dccfe600c7f2e7109dc3af4
Louvani <paula.louvani@gmail.com>
2021-03-08 21:33:31 -0500

    Liberando cada variable

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,20 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * free_dog - frees dogs
  * @d: my dog
  * Return: pointer (d)
  */
-
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
 	if (d != NULL)
-			free(d);
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+	}
+	free(d);
 }
