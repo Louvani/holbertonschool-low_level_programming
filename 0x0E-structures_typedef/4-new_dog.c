39fca5dd5b199490f3f22ae3e2d120fde341a797
Louvani <paula.louvani@gmail.com>
2021-03-09 12:40:25 -0500

    Task4 using _strdup function

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *_strdup(char *str);
+
 /**
  * new_dog - function that creates a new dog.
  * @name: me of the dog
@@ -12,15 +14,47 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *woof;
+	dog_t *dogs;
 
-	woof = malloc(sizeof(dog_t));
-	if (woof == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
+	dogs = malloc(sizeof(dog_t));
+	if (dogs == NULL)
+		return (NULL);
+	dogs->name = _strdup(name);
+	dogs->owner = _strdup(owner);
+	dogs->age = age;
 
-	woof->name = name;
-	woof->age = age;
-	woof->owner = owner;
+	return (dogs);
+}
 
-	return (woof);
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ * Return: Char.
+ */
+char *_strdup(char *str)
+{
+	char *a, *aux;
+	int l;
+
+	if (str == NULL)
+		return (NULL);
+	l = 0;
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	a = malloc(sizeof(char) * (l + 1));
+	if (a == NULL)
+		return (NULL);
+	aux = a;
+	while (*str != '\0')
+	{
+		*aux = *str;
+		aux++;
+		str++;
+	}
+	*aux = '\0';
+	return (a);
 }
