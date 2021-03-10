e5ac408822a54d0269e9a178b3576af5eb2e05c6
Louvani <paula.louvani@gmail.com>
2021-03-10 17:56:22 -0500

    Task1 i < size

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
  * array_iterator - executes a function given on each element of an array.
  * @array:A variable of an array
@@ -15,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	unsigned int i = 0;
 
-	while (i <= size)
+	while (i < size)
 	{
 		action(array[i]);
 		i++;
