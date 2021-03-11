cc354e93c2b2a5493da2f18972529b6f2ab91100
Louvani <paula.louvani@gmail.com>
2021-03-11 12:59:09 -0500

    Variadic_functions + vi

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,6 @@
 #define HOLB_H
 #include <stdio.h>
 #include <stdlib.h>
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
