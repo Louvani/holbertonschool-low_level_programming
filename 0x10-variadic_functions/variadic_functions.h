d5499eea2ad21273b47d9dfde83b6d3fea920d7b
Louvani <paula.louvani@gmail.com>
2021-03-11 12:55:56 -0500

    Archivos generales

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef HOLB_H
+#define HOLB_H
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /*HOLBERTON_H*/
