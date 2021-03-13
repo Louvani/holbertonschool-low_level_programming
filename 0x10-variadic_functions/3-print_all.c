32e8892c9bc0d117f75f23285ee0db05bf31fd9d
Louvani <paula.louvani@gmail.com>
2021-03-13 00:48:50 -0500

    Task 3 imprimiendo nil en vez de null

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,6 +10,7 @@ void print_string(va_list s);
  * print_all - function that prints strings, followed by a new line.
  * @format:_constante
  */
+
 void print_all(const char * const format, ...)
 {
 	fun frmts[] = {
@@ -29,11 +30,12 @@ void print_all(const char * const format, ...)
 	while (format[i] != '\0')
 	{
 		j = 0;
+		/*loop to compare format with frmt*/
 		while (frmts[j].str != NULL)
 		{
 			if (*(format + i) == frmts[j].str[0])
 			{
-				printf("%s", separator);
+				printf("%s", separator); /*way avoid print separator at the end*/
 				frmts[j].f(list);
 				separator = ", ";
 			}
@@ -76,9 +78,13 @@ void print_float(va_list f)
  */
 void print_string(va_list s)
 {
-	if (s == NULL)
-	{
-		printf("(nil)");
-	}
-	printf("%s", va_arg(s, char *));
+	char *a; /*variable to keep value of s*/
+
+	a = va_arg(s, char *);
+	/*
+	*if function recives a NULL change a for (nil)
+	*/
+	if (a == NULL)
+		a = "(nil)";
+	printf("%s", a);
 }
