0cadc90c7a7f16bd100aff64556dfaae33d83e8a
Louvani <paula.louvani@gmail.com>
2021-03-12 23:44:25 -0500

    Task3 Comments and separator

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,9 +7,8 @@ void print_float(va_list f);
 void print_string(va_list s);
 
 /**
- * print_strings - function that prints strings, followed by a new line.
- * @separator: string to separete words.
- * @n: number of arguments pased.
+ * print_all - function that prints strings, followed by a new line.
+ * @format:_constante
  */
 void print_all(const char * const format, ...)
 {
@@ -19,56 +18,60 @@ void print_all(const char * const format, ...)
 		{"f", print_float},
 		{"s", print_string},
 		{NULL, NULL}
-    };
+	};
 	int i = 0;
 	int j = 0;
 	va_list list;
+
 	va_start(list, format);
 
 	while (format[i] != '\0')
 	{
+		j = 0;
 		while (frmts[j].str != NULL)
 		{
 			if (*(format + i) == frmts[j].str[0])
+			{
 				frmts[j].f(list);
+				if (format[i] != '\0')
+					printf(", ");
+			}
 			j++;
 		}
+
 		i++;
 	}
+	printf("\n");
 	va_end(list);
 }
 
 /**
- * print_strings - function that prints strings, followed by a new line.
- * @separator: string to separete words.
- * @n: number of arguments pased.
+ * print_char - function that prints characters
+ * @c: character
  */
 void print_char(va_list c)
 {
 	printf("%c", va_arg(c, int));
 }
 /**
- * print_strings - function that prints strings, followed by a new line.
- * @separator: string to separete words.
- * @n: number of arguments pased.
+ * print_integer - function that prints numbers.
+ * @i: integer
  */
 void print_integer(va_list i)
 {
 	printf("%d", va_arg(i, int));
 }
 /**
- * print_strings - function that prints strings, followed by a new line.
- * @separator: string to separete words.
- * @n: number of arguments pased.
+ * print_float - function that prints floats
+ * @f: float
  */
 void print_float(va_list f)
 {
 	printf("%f", va_arg(f, double));
 }
 /**
- * print_strings - function that prints strings, followed by a new line.
- * @separator: string to separete words.
- * @n: number of arguments pased.
+ * print_string - function that prints strings
+ * @s: string
  */
 void print_string(va_list s)
 {
