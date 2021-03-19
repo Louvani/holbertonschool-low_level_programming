f9fec433f05211ce5c93f413f3383eafe7b76de9
Louvani <paula.louvani@gmail.com>
2021-03-18 20:35:30 -0500

    Project 0x12. C - Singly linked lists
    Add README file
    Add header file "list.h"
    Add task 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: pointer to head
+ * Return: The number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = NULL;
+	size_t counter = 0; /*counts the number of nodes*/
+
+	current = h;
+	while (current != NULL)
+	{
+		counter += 1;
+		if (current->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+	}
+	return (counter);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+# 0x12. C - Singly linked lists
+
+This project to learn about single liinked lists, when and why use them instead of arrays, and also how to build and use this linked lists
+
+This project consist of five mandatory tasks.
+- The first one to learn how to count the number of nodes and print them.
+- The second one to learnd how to return the number of elements in a linked list
+
