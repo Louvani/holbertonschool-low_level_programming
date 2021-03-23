621b9580fcc1dfceedc1c3fd6c15d2008bba484d
Louvani <paula.louvani@gmail.com>
2021-03-23 10:32:31 -0500

    Task 7 remove condition if index 1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -35,8 +35,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	if (index == 1)
-		return (temp);
+
 	if (index <= num_of_nodes)
 	{
 		while (counter < index && temp->next != NULL)
