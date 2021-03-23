710489af7ba81ac03903542fa472f07bbb23a8f6
Louvani <paula.louvani@gmail.com>
2021-03-23 10:08:21 -0500

    come back , needs two conditions

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *current = NULL; /*points to to the last node created*/
 	int value;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
 	current = *head;
