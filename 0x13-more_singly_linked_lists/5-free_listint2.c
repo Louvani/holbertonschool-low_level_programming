a1f60c4c45d2d758934c2ed6934ed6419a77fb5d
Louvani <paula.louvani@gmail.com>
2021-03-23 00:12:26 -0500

    task five return if head is NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * free_listint2 - function that frees a listint_t list.
  * @head: pointer to the head of the list
@@ -9,6 +8,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp = NULL;
 	listint_t *current = *head;
+
+	if (*head == NULL)
+		return;
 
 	while (current != NULL)
 	{
