e79c40f6e4b7f2083e34151c7beb51e256bdfbd5
Louvani <paula.louvani@gmail.com>
2021-03-26 19:46:23 -0500

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - returns num of bits needed to flip to get from one
+ * number to another.
+ * @n: first number to convert
+ * @m: second number
+ * Return: number of bits to set
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int aux; /*keep xor of n and m*/
+	int counter_of_difference = 0; /*count the number of diference*/
+
+	aux = (n ^ m); /*the result will be compauxre with aux-1*/
+
+	while (aux > 0)
+	{
+		counter_of_difference++;
+		aux &= (aux - 1);
+	}
+	return (counter_of_difference);
+}
