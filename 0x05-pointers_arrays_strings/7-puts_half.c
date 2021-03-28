b07d7b491a9097e2db7b83381d820c659a8b5982
Louvani <paula.louvani@gmail.com>
2021-03-28 17:36:23 -0500

    Project 0x05 task 7
    now print correctly, using an auxiliar variable and set the starting
    point.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,32 +4,29 @@
  *puts_half - function that prints every other character of a string
  *@str: String
  */
-
 void puts_half(char *str)
 {
-	int l;
+	int l = 0;
 	char *strAux = str;
-	int startingPoint;
+	int startingPoint; /*half of the string*/
 
-	l = 0;
-
+	/*get the lengt of the string*/
 	while (*strAux)
 	{
 		l++;
 		strAux++;
 	}
+	/*check if string is odd*/
 	if (l % 2 == 0)
-	{
 		startingPoint = l / 2;
-	}
 	else
-	{
 		startingPoint = (l - 1) / 2;
-	}
-	while (str[l] + startingPoint)
+	/*reset aux to start at the middle of the string*/
+	strAux = str + startingPoint;
+	while (*strAux)
 	{
-		_putchar(*str++);
-		startingPoint++;
+		_putchar(*strAux);
+		strAux++;
 	}
 	_putchar(10);
 }
