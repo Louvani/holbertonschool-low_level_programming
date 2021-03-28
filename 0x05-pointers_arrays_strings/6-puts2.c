0792caca97d5b59823a824e4221cb7fbcb77ce31
Louvani <paula.louvani@gmail.com>
2021-03-28 17:17:33 -0500

    Project 0x05 task6 now don't print extra characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,14 +4,17 @@
  *puts2 - function that prints every other character of a string
  *@str: String
  */
-
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		if ()
-			_putchar(*str);
-		str += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar(10);
 }
