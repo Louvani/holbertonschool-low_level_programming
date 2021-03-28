457c2726220fd4432d854e1602cbd2ba5c52717c
Louvani <paula.louvani@gmail.com>
2021-03-28 17:01:40 -0500

    Project 0x05 task5 reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,9 +4,25 @@
  *rev_string - Functión that prints a string, in reverse
  *@s: Variable
  */
+
 void rev_string(char *s)
 {
-	int l;
+	int lenght; /*of the string*/
+	char aux[1024];
+	int i = 0;
 
-	
+	lenght = 0;
+	/*Calaculate the lenght and and copy string in array aux*/
+	while (s[lenght] != '\0')
+	{
+		aux[lenght] = s[lenght];
+		lenght++;
+	}
+	lenght -= 1;
+	/*overwrite s from lastt position with aux that is in first position*/
+	for (; lenght >= 0; --lenght)
+	{
+		s[lenght] = aux[i];
+		i++;
+	}
 }
