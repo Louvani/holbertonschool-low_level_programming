664d9720e52859ecce558577e88ab8750f600eba
Louvani <paula.louvani@gmail.com>
2021-03-28 18:05:45 -0500

    Project 0x05 task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - function that prints n elements of an array of INTEGERS.
+ * @a: poiter to the array
+ * @n: is the number of elements of the array to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n - 1 && a != NULL)
+	{
+		printf("%d, ", a[i]);
+		i++;
+	}
+	printf("%d\n", a[i]);
+}
