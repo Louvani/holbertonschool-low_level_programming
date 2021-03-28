2c60adf8a4d551a4d514fb01bf1ba7f0ed146d0a
Louvani <paula.louvani@gmail.com>
2021-03-28 18:16:23 -0500

    Task 8 if n is -1 return

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,10 +6,11 @@
  * @a: poiter to the array
  * @n: is the number of elements of the array to be printed
  */
-
 void print_array(int *a, int n)
 {
 	int i = 0;
+	if (n == -1)
+		return;
 
 	while (i < n - 1 && a != NULL)
 	{
