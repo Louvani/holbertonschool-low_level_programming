8b6dfe0c4104bc1e3957cce27d3e0e7246b9ff2b
Louvani <paula.louvani@gmail.com>
2021-03-29 13:32:07 -0500

    task0 write whit the lenght of the buffer

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,20 @@
 #include "holberton.h"
+/**
+ * myStrlen - function that concatenates two strings.
+ * @p: variable to determine the dest lenght.
+ * Return: always count.
+ */
+unsigned int myStrlen(char *p)
+{
+	unsigned int count = 0;
 
+	while (*p != '\0')
+	{
+		count++;
+		p++;
+	}
+	return (count);
+}
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: file to be readed
@@ -11,7 +26,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_descriptor;
 	char *buffer;
+	int len_buffer;
 
+	/*open and read file*/
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
@@ -20,10 +37,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buffer)
 		return (0);
 	read(file_descriptor, buffer, letters);
-	buffer[letters + 1] = '\0';
 
 	close(file_descriptor);
-	write(1, buffer, letters + 1);
+	/*Put the text into an auxiliar variable*/
+	len_buffer = myStrlen(buffer);
+
+	buffer[len_buffer + 1] = '\0';
+
+	len_buffer = myStrlen(buffer);
+	write(1, buffer, len_buffer);
 	free(buffer);
-	return (letters);
+	return (len_buffer);
 }
