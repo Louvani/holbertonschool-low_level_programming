2444300052eef70848f044c8af5b72f8fbf737b4
Louvani <paula.louvani@gmail.com>
2021-03-28 20:04:34 -0500

    0x05 task 100 atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,7 +5,27 @@
  * @s: string
  * Return: an integer.
  */
+
 int _atoi(char *s)
 {
+	int number = 0;
+	int i = 0, sign = 1;
 
+	while (s[i])
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			if (s[i - 1] == '-')
+				sign = (-1);
+			number = number * 10 + s[i] - 48;
+			if (s[i + 1] <= 48 && s[i + 1] >= 57)
+			{
+				number = sign * number;
+				return (number);
+			}
+		}
+		i++;
+	}
+	number = sign * number;
+	return (number);
 }
