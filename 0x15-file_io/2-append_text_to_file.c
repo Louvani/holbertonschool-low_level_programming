dec63bf167ef95b8b25c32b94bb2cb4051cb8fcc
Louvani <paula.louvani@gmail.com>
2021-03-29 20:11:43 -0500

    Close fd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,6 +23,7 @@ unsigned int myStrlen(char *p)
  * Return: 1 on success, -1 on failure (file can not be created,
  * file can not be written, write “fails”, etc…)
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i, len;
@@ -30,16 +31,23 @@ int append_text_to_file(const char *filename, char *text_content)
 	len = myStrlen(text_content);
 	if (filename == NULL)
 		return (-1);
+
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
+
 	if (text_content == NULL)
+	{
+		close(fd);
 		return (1);
+	}
 	i = write(fd, text_content, len);
 		if (i == -1)
+		{
+			close(fd);
 			return (-1);
+		}
 
 	close(fd);
-
 	return (1);
 }
