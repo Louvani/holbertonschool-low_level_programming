c31fd391097a78ae6626d7039f4748af38f4141d
Louvani <paula.louvani@gmail.com>
2021-03-29 19:37:12 -0500

    Do not use dprintf--> now write again

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,21 @@
 #include "holberton.h"
+/**
+ * myStrlen - function that concatenates two strings.
+ * @p: variable to determine the dest lenght.
+ * Return: always count.
+ */
 
+unsigned int myStrlen(char *p)
+{
+	unsigned int count = 0;
+
+	while (*p != '\0')
+	{
+		count++;
+		p++;
+	}
+	return (count);
+}
 /**
  * append_text_to_file -  appends text at the end of a file
  * @filename:is the name of the file to create.
@@ -7,16 +23,18 @@
  * Return: 1 on success, -1 on failure (file can not be created,
  * file can not be written, write “fails”, etc…)
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, i, len;
 
+	len = myStrlen(text_content);
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-	i = dprintf(fd, "%s", text_content);
+	i = write(fd, text_content, len);
 	if (i == -1)
 		return (-1);
 	close(fd);
