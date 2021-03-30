97ff3bf65773eedfd2c7152183e1f115832772da
Louvani <paula.louvani@gmail.com>
2021-03-30 15:33:05 -0500

    Task3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * main - program that copies the content of a file to another file.
+ * @argc:Counter of arguments
+ * @argv: arguments
+ * Return: 0 if have more than 1 arguments.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, read_result, write_result;
+	int close1, close2;
+	char buffer[1024];
+
+	if (argc != 3)
+	{	printf("Usage: cp file_from file_to");
+		exit(97);
+	}
+	if (argv[1] == NULL)
+	{	printf("Error: Can't read from file %s", argv[1]);
+		exit(98);
+	}
+	/*open files*/
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1 || file_from == -1)
+		return (0);
+	/*copy on the second file*/
+	while ((read_result = read(file_from, buffer, 1024) > 0))
+	{
+		write_result = dprintf(file_to, "%s", buffer);
+		if (write_result == -1)
+		{
+			printf("Error: Can't write to %s", argv[1]);
+			exit(99);
+		}
+	}
+	close1 = close(file_from);
+	if (close1 == -1)
+	{
+		printf("Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	close2 = close(file_to);
+	if (close2 == -1)
+	{
+		printf("Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
