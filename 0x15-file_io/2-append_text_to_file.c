5a1605aac5b338e6e0755f009650b47e1ca7241b
Louvani <paula.louvani@gmail.com>
2021-03-29 20:20:42 -0500

    add comments

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,19 +23,20 @@ unsigned int myStrlen(char *p)
  * Return: 1 on success, -1 on failure (file can not be created,
  * file can not be written, write “fails”, etc…)
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i, len;
+	int fd; /*file descriptor*/
+	int i;
+	int len; /*lenght of the text_content*/
 
 	len = myStrlen(text_content);
 	if (filename == NULL)
 		return (-1);
-
+	/*open the file and check*//*Do not create the file if it does not exist*/
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-
+	/*If text_content is NULL, do not add anything to the file. Return 1 */
 	if (text_content == NULL)
 	{
 		close(fd);
