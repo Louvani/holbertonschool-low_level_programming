ef48e1c34f5df20785a2ce774f2837f71f7e30c6
Louvani <paula.louvani@gmail.com>
2021-03-29 19:34:04 -0500

    flag wronly for the open function

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,18 +7,18 @@
  * Return: 1 on success, -1 on failure (file can not be created,
  * file can not be written, write “fails”, etc…)
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-
-	dprintf(fd, "%s", text_content);
+	i = dprintf(fd, "%s", text_content);
+	if (i == -1)
+		return (-1);
 	close(fd);
 
 	return (1);
