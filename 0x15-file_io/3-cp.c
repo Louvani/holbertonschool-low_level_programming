6217a5068529d503a6bc3771e26483be90072473
Louvani <paula.louvani@gmail.com>
2021-03-31 15:35:26 -0500

    Task 3 close before exits

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,6 @@
  * @argv: arguments
  * Return: 0 if have more than 1 arguments.
  */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, read_result, write_result;
@@ -17,10 +16,6 @@ int main(int argc, char *argv[])
 	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (argv[1] == NULL)
-	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	/*open files*/
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
@@ -30,6 +25,7 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		close1 = close(file_from);
 		exit(99);
 	}
 	/*copy on the second file*/
@@ -38,12 +34,16 @@ int main(int argc, char *argv[])
 		if (read_result == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close1 = close(file_from);
+			close2 = close(file_to);
 			exit(98);
 		}
+		close1 = close(file_from);
 		write_result = dprintf(file_to, "%s", buffer);
 		if (write_result == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+			close2 = close(file_to);
 			exit(99);
 		}
 	}
