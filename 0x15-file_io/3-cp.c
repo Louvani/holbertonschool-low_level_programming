c764095748a247c740cabb96e7413332fd273251
Louvani <paula.louvani@gmail.com>
2021-03-30 20:15:43 -0500

    Veryfy all the conditions for exit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,6 @@
  * @argv: arguments
  * Return: 0 if have more than 1 arguments.
  */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, read_result, write_result;
@@ -14,25 +13,36 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	if (argv[1] == NULL)
-	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	/*open files*/
 	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to == -1 || file_from == -1)
-		return (0);
+	if (file_to == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(99);
+	}
 	/*copy on the second file*/
 	while ((read_result = read(file_from, buffer, 1024) > 0))
 	{
+		if (read_result == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		write_result = dprintf(file_to, "%s", buffer);
 		if (write_result == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 			exit(99);
 		}
 	}
