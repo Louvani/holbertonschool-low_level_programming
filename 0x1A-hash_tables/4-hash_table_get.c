f13d4dffec3b7679c51ecf552eeec60c1fc00d1b
Louvani <paula.louvani@gmail.com>
2021-05-28 15:16:17 -0500

    vim

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 
 	if (key == NULL)
-		return (0);
+		return (NULL);
 
 	/*Get the index of in the aarray*/
 	index = key_index((unsigned char *)key, ht->size);
