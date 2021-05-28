6a50ef96cf7a0f2f5b6534d0b1bf9f38bd3457cd
Louvani <paula.louvani@gmail.com>
2021-05-28 13:04:54 -0500

    Strlen of key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_element;
 	unsigned long int index;
 
-	if (key == NULL)
+	if (key == NULL || strlen(key) == 0)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
