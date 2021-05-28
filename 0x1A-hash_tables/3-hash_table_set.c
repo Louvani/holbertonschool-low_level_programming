da4888673549833928363b2ce7ac0d15e98edf1e
Louvani <paula.louvani@gmail.com>
2021-05-28 12:11:14 -0500

    Condition for empty key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,6 +14,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_element;
 	unsigned long int index;
 
+	if (key == NULL || key == "")
+		return (0);
+
 	index = key_index((unsigned char *)key, ht->size);
 
 	new_element = malloc(sizeof(hash_node_t));
