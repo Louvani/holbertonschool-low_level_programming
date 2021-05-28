eb3a445b4deada1962bdfebdfb3c849243a81bbc
Louvani <paula.louvani@gmail.com>
2021-05-28 09:20:43 -0500

    Task1

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 
 /*INCLUDES*/
 #include <stdlib.h> /*Malloc*/
+#include <stdio.h>
 
 
 /**
@@ -36,5 +37,6 @@ typedef struct hash_table_s
 
 /*Prototypes*/
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
