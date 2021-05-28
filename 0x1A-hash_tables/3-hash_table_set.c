284bd3b94b8da85eb8f4ecdb44875481993feb4a
Louvani <paula.louvani@gmail.com>
2021-05-28 14:48:21 -0500

    Checks if value exist

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,23 +12,42 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_element;
+	hash_node_t *aux_node; /*to check if a key alrready exist*/
 	unsigned long int index;
 
 	if (key == NULL || strlen(key) == 0 || !ht || !value)
 		return (0);
-
+	/*Get the index of in the aarray*/
 	index = key_index((unsigned char *)key, ht->size);
-
+	/*If key alrredy exist update the value and return*/
+	aux_node = ht->array[index];
+	while (aux_node)
+	{
+		if (strcmp(aux_node->key, key) == 0)
+		{
+			free(aux_node->value);
+			aux_node->value = strdup(value);
+			return (1);
+		}
+		aux_node = aux_node->next;
+	}
+	/*create the new node*/
 	new_element = malloc(sizeof(hash_node_t));
 	if (new_element == NULL)
 		return (0);
-
+printf("llego\n");
+	/*Set values for the new node*/
+	new_element->next = NULL;
 	new_element->key = (char *)key;
 	new_element->value = strdup(value);
-	new_element->next = NULL;
+	if (!new_element->key)
+	{
+		free(new_element);
+		return (0);
+	}
+	/*inset node in the list*/
 	if (ht->array[index])
 		new_element->next = ht->array[index];
 	ht->array[index] = new_element;
-
 	return (1);
 }
