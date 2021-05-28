29430765b63f465cf63b4c176022b0d132549386
Louvani <paula.louvani@gmail.com>
2021-05-28 15:00:14 -0500

    Create node in another function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,8 +11,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_element;
-	hash_node_t *aux_node; /*to check if a key alrready exist*/
+	hash_node_t *new_element, *aux_node;
 	unsigned long int index;
 
 	if (key == NULL || strlen(key) == 0 || !ht || !value)
@@ -27,14 +26,37 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(aux_node->value);
 			aux_node->value = strdup(value);
+
 			return (1);
 		}
 		aux_node = aux_node->next;
 	}
 	/*create the new node*/
+	new_element = create_node(key, value);
+	if (!new_element)
+		return (0);
+	/*inset node in the list*/
+	new_element->next = ht->array[index];
+	ht->array[index] = new_element;
+	return (1);
+}
+
+/**
+ * create_node - create the node
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key. value must be duplicated
+ * value can be an empty string.
+ * Return: the new node
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_element;
+
+	/*create the new node*/
 	new_element = malloc(sizeof(hash_node_t));
 	if (new_element == NULL)
-		return (0);
+		return (NULL);
+
 	/*Set values for the new node*/
 	new_element->next = NULL;
 	new_element->key = (char *)key;
@@ -42,11 +64,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_element->key)
 	{
 		free(new_element);
-		return (0);
+		return (NULL);
 	}
-	/*inset node in the list*/
-	if (ht->array[index])
-		new_element->next = ht->array[index];
-	ht->array[index] = new_element;
-	return (1);
+	return (new_element);
 }
